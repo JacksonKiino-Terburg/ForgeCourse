56f7b7a24130ee1c4a31af796245fb92eca034fb
EnderOwlPlays <jjkiino@gmail.com>
2024-05-01 22:55:16 -0400

    added alexandrite and corresponding ores

--- src/main/java/net/berlitz/mccourse/MCCourseMod.java
+++ src/main/java/net/berlitz/mccourse/MCCourseMod.java
@@ -1,6 +1,8 @@
 package net.berlitz.mccourse;
 
 import com.mojang.logging.LogUtils;
+import net.berlitz.mccourse.block.ModBlocks;
+import net.berlitz.mccourse.item.ModCreativeModeTabs;
 import net.berlitz.mccourse.item.ModItems;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
@@ -29,7 +31,10 @@ public class MCCourseMod
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        ModCreativeModeTabs.register(modEventBus);
+
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
@@ -49,6 +54,15 @@ public class MCCourseMod
         if(event.getTabKey() == CreativeModeTabs.INGREDIENTS){
             event.accept(ModItems.ALEXANDRITE);
             event.accept(ModItems.RAW_ALEXANDRITE);
+        }
+
+        if(event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS){
+            event.accept(ModBlocks.ALEXANDRITE_BLOCK);
+            event.accept(ModBlocks.RAW_ALEXANDRITE_BLOCK);
+            event.accept(ModBlocks.ALEXANDRITE_ORE);
+            event.accept(ModBlocks.DEEPSLATE_ALEXANDRITE_ORE);
+            event.accept(ModBlocks.NETHER_ALEXANDRITE_ORE);
+            event.accept(ModBlocks.END_STONE_ALEXANDRITE_ORE);
         }
     }
 
